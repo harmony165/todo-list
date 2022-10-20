d788f97b840a042365e4f38652ba9c94204cd695
kaushik.s@kristasoft.com <kaushik.s@kristasoft.com>
2022-10-20 21:59:32 +0530

    symantic improvement

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,6 @@
   import TodoContainer from "./lib/TodoContainer.svelte";
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css"
-  />
-</svelte:head>
-
 <main>
   <div>
     <h1>Day Planner</h1>
